c14f7b071990336f090e7998f53b3244c2a267eb
Stephen Li <stephenli.ca@gmail.com>
2014-10-18 17:57:31 -0400

    Updated script

--- scripts/dev_setup_linux.bash
+++ scripts/dev_setup_linux.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Pip and all the other python dependencies
+sudo apt-get install -y python
+sudo apt-get install -y python-pip
+sudo pip install -r ../requirements.txt
+
 # Postgres
 sudo apt-get install -y postgresql
-sudo apt-get install python-psycopg2
-
-# Pip and all the other python dependencies
-sudo apt-get install -y python-pip
-pip install -r ../requirements.txt
+sudo apt-get install -y python-psycopg2
 
 # Misc
 sudo npm install -g less
