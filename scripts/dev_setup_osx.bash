43330e5ce587369403ec464a8e4f5f1ba409daf0
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-10-19 13:51:57 -0400

    Adding comments to the os x install script

--- scripts/dev_setup_osx.bash
+++ scripts/dev_setup_osx.bash
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# Install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 export PATH="/usr/local/bin:$PATH"
 source ~/.bash_profile
 
+# Ensure brew is up to date
 brew update
 
+# Install Python + dependencies
 brew install python
 sudo pip install -r ../requirements.txt
 
 # Install Postgres
 brew install postgresql
 
+# Install npm 
 brew install npm
 
+# Install less compiler
 sudo npm install -g less
