37ce8fd1b1a1107362bb547a65d2cd88af6413f4
Connor Cimowsky <connorcimowsky@me.com>
2014-10-19 01:55:04 -0400

    Fix the directories for static files.

--- sana_builder/sana_builder/settings.py
+++ sana_builder/sana_builder/settings.py
@@ -103,8 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 #------------------------------------------------------------------------------
 
-STATIC_ROOT = 'static/'
-STATIC_URL  = os.path.join(BASE_DIR, 'static/')
+STATIC_URL  = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Compile less
 COMPRESS_PRECOMPILERS = (
