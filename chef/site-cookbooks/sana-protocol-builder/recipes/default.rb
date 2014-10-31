eba24b68f2bf1cf9ed2b579a00062252351add99
Connor Cimowsky <connorcimowsky@me.com>
2014-10-31 13:34:33 -0400

    Add a custom cookbook for Sana Protocol Builder.

---
+++ chef/site-cookbooks/sana-protocol-builder/recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: sana-protocol-builder
+# Recipe:: default
+#
+
+git '/opt/' do
+  repository 'git@github.com:SanaMobile/sana.protocol_builder.git'
+  revision 'master'
+  action :sync
+end
+
+cookbook_file '/etc/nginx/sites-available/sanaprotocolbuilder.me.conf' do
+  source 'sanaprotocolbuilder.me.conf'
+  action :create
+end
+
+link '/etc/nginx/sites-enabled/sanaprotocolbuilder.me.conf' do
+  to '/etc/nginx/sites-available/sanaprotocolbuilder.me.conf'
+end
+
+service 'nginx' do
+  action [:enable, :start, :reload]
+end
+
+supervisor_service 'gunicorn' do
+  autostart true
+  autorestart true
+
+  command '/root/.virtualenvs/sana_protocol_builder/bin/gunicorn sana_builder.wsgi:application -c config/gunicorn.conf.py'
+  directory '/opt/sana.protocol_builder'
+  environment 'PATH="/root/.virtualenvs/sana_protocol_builder/bin"'
+
+  redirect_stderr true
+end
