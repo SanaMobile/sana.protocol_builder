6a4e5001b8fe6b8652f7a092dee6e583bfd4cd2b
Connor Cimowsky <connorcimowsky@me.com>
2014-10-31 13:47:03 -0400

    Escape quotes.

--- chef/site-cookbooks/sana-protocol-builder/recipes/default.rb
+++ chef/site-cookbooks/sana-protocol-builder/recipes/default.rb
@@ -28,7 +28,7 @@ supervisor_service 'gunicorn' do
 
   command '/root/.virtualenvs/sana_protocol_builder/bin/gunicorn sana_builder.wsgi:application -c config/gunicorn.conf.py'
   directory '/opt/sana.protocol_builder'
-  environment 'PATH="/root/.virtualenvs/sana_protocol_builder/bin"'
+  environment 'PATH=\"/root/.virtualenvs/sana_protocol_builder/bin\"'
 
   redirect_stderr true
 end
