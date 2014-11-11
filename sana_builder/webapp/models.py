e977331dff6fc4f03eef42be504ec3d747e3f9ad
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-11-10 22:48:29 -0500

    Create a Procedure model and an example Page model
    Page only contains a foreign key to a procedure for now.

--- sana_builder/webapp/models.py
+++ sana_builder/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Procedure(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    uuid = models.IntegerField(null=True)
+    version = models.CharField(max_length=50, null=True)
+    owner = models.ForeignKey(User, unique=True)
+
+class Page(models.Model):
+    procedure = models.ForeignKey(Procedure)
