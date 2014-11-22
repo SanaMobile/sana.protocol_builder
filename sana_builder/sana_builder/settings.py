2e719387ee1c0e2214cfe7c162f17eca49e8e535
Joshua Kalpin <joshua.kalpin@gmail.com>
2014-11-21 19:50:35 -0500

    Add Nose as the unit testing framework
    Test running is now a normal command vs. a weird path

--- sana_builder/sana_builder/settings.py
+++ sana_builder/sana_builder/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = (
     'guardian',                     # Object-level permissions
     'rest_framework',               # RESTful endpoint support
     'rest_framework.authtoken',     # Token based authentication
+    'django_nose',                  # Better test framework/runner
 
     # Our apps
     'webapp'
@@ -175,3 +176,9 @@ COMPRESS_PRECOMPILERS = (
 STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
+
+#------------------------------------------------------------------------------
+# Test Configuration
+#------------------------------------------------------------------------------
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
--- sana_builder/webapp/tests/test_example.py
+++ sana_builder/webapp/tests/test_example.py
@@ -1,17 +1,8 @@
-from unittest import TestCase
+def test_that_always_passes():
+    val1 = 10
+    val2 = 10
+    val3 = 'foo'
 
-class ExampleTest(TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_that_always_passes(self):
-        val1 = 10
-        val2 = 10
-        val3 = 'foo'
-
-        self.assertEqual(val1, val2)
-        self.assertNotEqual(val1, val3)
+    assert val1 == val2
+    assert val1 != val3
 
