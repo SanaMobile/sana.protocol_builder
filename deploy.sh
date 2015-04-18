1a1d251d08bd3a56b5714a16c7e0f76b92c54dd7
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-04-18 00:34:07 -0700

    Actually see output

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
-if [ "$TRAVIS_BRANCH" == "master" ] &&
-   [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Deploying!"
+#!/bin/bash
 
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     openssl aes-256-cbc -K $encrypted_a0fbdc5ae723_key -iv $encrypted_a0fbdc5ae723_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
     chmod 600 .travis/deploy_key.pem
     eval "$(ssh-agent)"
     ssh-add .travis/deploy_key.pem
     fab travis_deploy
-else
-    echo "Pull-Request. Will not deploy."
 fi
