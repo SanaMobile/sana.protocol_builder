97eab414379b2763a9183083113aa51990364eaa
Connor Cimowsky <connorcimowsky@me.com>
2015-05-23 12:21:44 -0400

    Add showModal and hideModal actions to the application route

--- src-frontend/app/routes/application.js
+++ src-frontend/app/routes/application.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    showModal: function(name) {
+      this.render(name, {
+        into: 'application',
+        outlet: 'modal'
+      });
+    },
+    hideModal: function() {
+      this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
+  }
 });
