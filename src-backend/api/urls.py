b3befb47d4b48e83b42fc6b10a10269d32cafb4e
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-05-23 22:42:04 -0400

    Remove trailing slash from the router

--- src-backend/api/urls.py
+++ src-backend/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from views import ProcedureViewSet
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 
 router.register(r'procedures', ProcedureViewSet)
 
