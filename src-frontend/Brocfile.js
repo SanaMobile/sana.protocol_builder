d7e7068c203561de59b9a89fc9412fcbd75c7c08
Connor Cimowsky <connorcimowsky@me.com>
2015-05-25 15:15:39 -0400

    Read authorization token from cookie

--- src-frontend/Brocfile.js
+++ src-frontend/Brocfile.js
@@ -11,6 +11,7 @@ var app = new EmberApp({
 });
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/js-cookie/src/js.cookie.js');
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
