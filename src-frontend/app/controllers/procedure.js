c8b620063409688ac8328d2a0911ba3cce598caa
Connor Cimowsky <connorcimowsky@me.com>
2015-05-25 12:41:24 -0400

    Allow adding pages/elements

--- src-frontend/app/controllers/procedure.js
+++ src-frontend/app/controllers/procedure.js
@@ -1,28 +1,48 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
   pages: function() {
-    return this.get('model.pages').sortBy('index');
+    return this.get('model.pages').sortBy('displayIndex');
   }.property('model.pages'),
 
   actions: {
+    addElement: function(page, selectedIndex) {
+      var targetIndex = selectedIndex + 1;
+      var elements = page.get('elements');
+      var elementsToBePushed = elements.filter(function(element) {
+        return element.get('displayIndex') >= targetIndex;
+      });
+
+      elementsToBePushed.forEach(function(element) {
+        element.incrementProperty('displayIndex');
+        element.save();
+      });
+
+      var elementInfo = {
+        page: page,
+        displayIndex: targetIndex
+      };
+
+      this.send('showModal', 'add-element-modal', elementInfo);
+    },
     addPage: function(selectedIndex) {
       var targetIndex = selectedIndex + 1;
       var pages = this.get('model.pages');
       var pagesToBePushed = pages.filter(function(page) {
-        return page.get('index') >= targetIndex;
+        return page.get('displayIndex') >= targetIndex;
       });
 
       pagesToBePushed.forEach(function(page) {
-        page.incrementProperty('index');
+        page.incrementProperty('displayIndex');
+        page.save();
       });
 
       var newPage = this.store.createRecord('page', {
-        index: targetIndex,
-        procedure: this.get('model')
+        procedure: this.get('model'),
+        displayIndex: targetIndex
       });
 
-      pages.addObject(newPage);
+      newPage.save();
     }
   }
 });
