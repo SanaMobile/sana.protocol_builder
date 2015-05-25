10465c3ca6ab24d538a6d4f4eef28379fb0a5cd0
Connor Cimowsky <connorcimowsky@me.com>
2015-05-25 12:34:27 -0400

    Update models

--- src-frontend/app/models/element.js
+++ src-frontend/app/models/element.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  index: DS.attr(),
-  type: DS.attr(),
-  concept: DS.attr(),
-  question: DS.attr(),
-  answer: DS.attr(),
+  page: DS.belongsTo('page'),
+  eid: DS.attr('string'),
+  displayIndex: DS.attr('number'),
+  elementType: DS.attr('string'),
+  concept: DS.attr('string'),
+  question: DS.attr('string'),
+  answer: DS.attr('string'),
   choices: DS.attr()
 });
--- src-frontend/app/models/procedure.js
+++ src-frontend/app/models/procedure.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr(),
+  title: DS.attr('string'),
   pages: DS.hasMany('page')
 });
