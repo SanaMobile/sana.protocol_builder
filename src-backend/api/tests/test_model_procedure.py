e154c6e8887ce0ce0e4c936e3387492db946f333
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-05-31 23:56:01 -0400

    Test the procedure's owner

--- src-backend/api/tests/test_model_procedure.py
+++ src-backend/api/tests/test_model_procedure.py
@@ -27,6 +27,7 @@ class ProcedureTest(TestCase):
         assert_equals(proc.title, 'test procedure')
         assert_equals(proc.version, None)
         assert_equals(proc.uuid, None)
+        assert_equals(proc.owner, self.test_user)
 
     def test_procedures_all_properties(self):
         test_uuid = str(uuid.uuid1())
@@ -44,6 +45,7 @@ class ProcedureTest(TestCase):
         assert_equals(proc.title, 'full test procedure')
         assert_equals(proc.version, '1.0')
         assert_equals(proc.uuid, test_uuid)
+        assert_equals(proc.owner, self.test_user)
 
     @raises(IntegrityError)
     def test_author_none(self):
