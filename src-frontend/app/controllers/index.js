2da9c6163765f03b0a344902ec3a9e2d093b1727
Fasih Awan <fasihawan@gmail.com>
2015-06-05 18:34:51 -0400

    Removed controller code

--- src-frontend/app/controllers/index.js
+++ src-frontend/app/controllers/index.js
@@ -1,10 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    beforeModel: function() {
-        debugger
-        if (Cookies.get('authorizationToken')) {
-            console.log("Has token");
-        }
-    }
 });
