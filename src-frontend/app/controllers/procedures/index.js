4ce57c72bb9172f3dfa24dc1a1ef1c7f6441316c
Connor Cimowsky <connorcimowsky@me.com>
2015-06-06 00:17:00 -0400

    Add procedure listing, plus a button for creating procedures

---
+++ src-frontend/app/controllers/procedures/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    actions: {
+        createProcedure: function() {
+            var procedureController = this;
+            var procedure = this.store.createRecord('procedure', {
+                title: 'Surgery Follow-Up',
+                author: 'Partners for Care',
+                owner: 1
+            });
+
+            procedure.save().then(function() {
+                procedureController.transitionToRoute('procedure', procedure);
+            });
+        }
+    }
+});
