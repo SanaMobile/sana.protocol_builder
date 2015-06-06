ee66d7cd21b55b68038ab214f3298b5f33ad57ea
Fasih Awan <fasihawan@gmail.com>
2015-06-05 21:43:11 -0400

    Added back some removed code

--- src-frontend/app/routes/application.js
+++ src-frontend/app/routes/application.js
@@ -2,6 +2,21 @@ import Ember from "ember";
 
 export default Ember.Route.extend({
     actions: {
+        showModal: function(name, model) {
+            this.render(name, {
+                into: 'application',
+                outlet: 'modal',
+                model: model
+            });
+        },
+        hideModal: function() {
+            this.disconnectOutlet({
+                outlet: 'modal',
+                parentView: 'application'
+            });
+        }
+    },
+    actions: {
         error: function(error, transition) {
             // If unauthorized
             if (error.status == 401) {
