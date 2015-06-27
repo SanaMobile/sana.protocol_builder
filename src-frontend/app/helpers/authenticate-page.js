a64f243e14e0b0c4f02183947f7fe399412bd8b2
Connor Cimowsky <connorcimowsky@me.com>
2015-06-26 21:22:00 -0400

    Rip out authenticate-page helper

--- src-frontend/app/helpers/authenticate-page.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Handlebars.makeBoundHelper(function(ctx) {
-    if (!Cookies.get('authorizationToken')) {
-        ctx.transitionTo('login');
-    }
-});
