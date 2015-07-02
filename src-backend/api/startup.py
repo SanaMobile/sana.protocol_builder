986f5bb4c0d152a88746ae0132d1a2739f6b0fa4
Stephen Li <stephenli.ca@gmail.com>
2015-07-02 14:09:33 -0400

    Fix syncdb on clean database
    We cannot run database operations in AppConfig.ready() because the
    models have not been created yet. The only place to put these one-time
    operations is in urls.py as it's the only other file that's loaded once
    and is loaded after database fully initialized.

---
+++ src-backend/api/startup.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from models import Procedure, Page, Element
+
+
+def __grant_permissions():
+    # Grant model permissions to the appropriate user groups
+    normal_users_group, created = Group.objects.get_or_create(name=settings.NORMAL_USER_GROUP)
+    models = [Procedure, Page, Element]
+
+    for m in models:
+        content_type = ContentType.objects.get_for_model(m)
+        permissions = Permission.objects.filter(content_type=content_type)
+
+        for p in permissions:
+            normal_users_group.permissions.add(p)
+
+
+def run_once():
+    __grant_permissions()
--- src-backend/api/urls.py
+++ src-backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
-from views import ProcedureViewSet, PageViewSet, ElementViewSet
 from rest_framework import routers
+from views import ProcedureViewSet, PageViewSet, ElementViewSet
 from api.signals import page_pre_save  # noqa
+from startup import run_once
+
 
 router = routers.SimpleRouter()
-
 router.register(r'procedures', ProcedureViewSet, base_name='procedure')
 router.register(r'pages', PageViewSet, base_name='page')
 router.register(r'elements', ElementViewSet, base_name='element')
@@ -12,3 +13,16 @@ router.register(r'elements', ElementViewSet, base_name='element')
 urlpatterns = [
     url(r'^', include(router.urls))
 ]
+
+
+# ------------------------------------------------------------------------------
+# Non-url related operations
+#
+# These must be run once on startup but need database access, otherwise they
+# can be put in __init__.py or AppConfig.ready()
+#
+# See http://stackoverflow.com/a/16111968
+#     https://docs.djangoproject.com/en/1.8/ref/applications/#django.apps.AppConfig.ready
+# ------------------------------------------------------------------------------
+
+run_once()
