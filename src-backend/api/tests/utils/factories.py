25b7a1a7e902c3bc70b62d18dc6031af8e5080e6
Joshua Kalpin <joshua.kalpin@gmail.com>
2015-07-03 18:43:50 -0400

    Add factory boy to make tests less gross looking.
    This updates the non-model tests to use factory boy. It makes things
    nice and pretty.

---
+++ src-backend/api/tests/utils/factories.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from api import models
+import factory
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ('username',)
+
+    username = 'TestUser'
+    email = 'test@sanaprotocolbuilder.me'
+    password = 'testpassword'
+
+
+class ProcedureFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Procedure
+
+    title = 'Sana Protocol Builder Procedure'
+    author = 'John Smith'
+    owner = factory.SubFactory(UserFactory)
+
+
+class PageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Page
+
+    display_index = 0
+    procedure = factory.SubFactory(ProcedureFactory)
+
+
+class ElementFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Element
+
+    display_index = 0
+    element_type = 'ENTRY'
+    concept = 'CHECKUP'
+    question = 'Where does it hurt?'
+    eid = 'checkup_id'
+    answer = ''
+    page = factory.SubFactory(PageFactory)
