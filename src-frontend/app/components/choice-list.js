bcd7a222bb71bc587000b6a781c20e7864bbb2a4
Connor Cimowsky <connorcimowsky@me.com>
2015-07-04 16:52:25 -0400

    Add sorting functionality to choice-list

--- src-frontend/app/components/choice-list.js
+++ src-frontend/app/components/choice-list.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    didInsertElement: function() {
+        var choiceListComponent = this;
+        this.$("ul.choices").sortable({
+            revert: true,
+            scroll: true,
+            update: function(event, ui) {
+                var choices = $(this).sortable('toArray', { attribute: 'choice' });
+
+                $(this).sortable('cancel');
+
+                choiceListComponent.sendAction('updateChoiceOrder', choices);
+            }
+        });
+    },
 });
