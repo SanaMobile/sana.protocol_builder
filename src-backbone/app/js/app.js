20cde3233da4ed8853fde124e4e46b2c52f7d1b2
Stephen Li <stephenli.ca@gmail.com>
2015-10-14 20:46:34 -0400

    Skeleton web app for Backbone frontend
    This is an empty skeleton app for a Backbone app using Gulp and
    Browserify (Coffeeify). We can use both CoffeeScript/JavaScript if we
    need to.
    
    Make sure you run `bower install` and `npm install` in the src-backbone
    directory to install all the dependencies. Also need to install these
    node packages globally:
    
        npm install -g bower
        npm install -g gulp
        npm install -g coffee-script
    
    Then you can simply run `gulp` to serve up a static page at
    localhost:8080.
    
    Note that bower components need to be stictly JS packages. I haven't
    configured gulp to pull out CSS/fonts found in some packages such as
    Bootstrap.

---
+++ src-backbone/app/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  
+  console.log(_);
+  console.log(Backbone);
+  console.log(require('./bar.js'));
+
+})
+
---
+++ src-backbone/gulpfile.js
@@ -0,0 +1,116 @@
+'use strict';
+
+//------------------------------------------------------------------------------
+// Config
+//------------------------------------------------------------------------------
+
+var gulp = require('gulp');
+
+var config = {
+  app: 'app/js/app.js',
+  javascripts: [
+    'app/js/**/*.js',
+  ],
+  coffeescripts: [
+    'app/js/**/*.coffee',
+  ],
+  images: [
+    'app/img/**/*'
+  ],
+  stylesheets: [
+    'app/css/**/*.less', // Since LESS is a superset of CSS, the preprocessor shouldn't affect regular CSS files
+    'app/css/**/*.css',
+  ],
+  bower: 'bower_components/**',
+  output: 'app/assets'
+};
+
+//------------------------------------------------------------------------------
+// Webserver
+//------------------------------------------------------------------------------
+
+var connect = require('gulp-connect');
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+    root: ['app']
+  });
+});
+
+//------------------------------------------------------------------------------
+// CSS Tasks
+//------------------------------------------------------------------------------
+
+var less = require('gulp-less');
+var minify_css = require('gulp-minify-css');
+var source_maps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+gulp.task('css', function() {
+  gulp.src(config.stylesheets)
+    .pipe(source_maps.init())
+    .pipe(less())
+    .pipe(concat('app.css'))
+    .pipe(minify_css())
+    .pipe(source_maps.write())
+    .pipe(gulp.dest(config.output))
+    .pipe(connect.reload());
+});
+
+//------------------------------------------------------------------------------
+// Bower Tasks
+//------------------------------------------------------------------------------
+
+var main_bower_files = require('main-bower-files');
+var uglify = require('gulp-uglify');
+gulp.task('bower', function() {
+  gulp.src(main_bower_files())
+    .pipe(concat('libs.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.output));
+});
+
+//------------------------------------------------------------------------------
+// JS Tasks
+//------------------------------------------------------------------------------
+
+var jshint = require('gulp-jshint'); // Note: we're using JSHint, a better fork of the original JSLint
+var stylish = require('jshint-stylish');
+gulp.task('js-lint', function() {
+  gulp.src(config.javascripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+var coffeelint = require('gulp-coffeelint');
+gulp.task('cs-lint', function() {
+  gulp.src(config.coffeescripts)
+    .pipe(coffeelint())
+    .pipe(coffeelint.reporter(stylish));
+});
+
+var coffeeify = require('gulp-coffeeify');
+gulp.task('js-coffee', function() {
+  gulp.src(config.app)
+    .pipe(coffeeify({
+      options: {
+        debug: true, // Generate source map 
+      }
+    }))
+    .pipe(gulp.dest(config.output))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', ['js-lint', 'js-coffee']);
+
+//------------------------------------------------------------------------------
+// Default
+// This executes when you run 'gulp' on the command line
+//------------------------------------------------------------------------------
+ 
+gulp.task('watch', function() {
+  gulp.watch(config.stylesheets, ['css']);
+  gulp.watch(config.javascripts, ['js']);
+  gulp.watch(config.bower, ['bower']);
+});
+
+gulp.task('default', ['css', 'js', 'bower', 'watch', 'webserver']);
