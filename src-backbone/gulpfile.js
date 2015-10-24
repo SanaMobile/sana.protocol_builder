75328ad38285c952abc1708ebb8301700a571e5d
Stephen Li <stephenli.ca@gmail.com>
2015-10-24 19:31:08 -0400

    Reconfigured gulp to allow css/fonts in bower packages
    - Changed gulpfile indentation to 4 spaces
    - Generated output gets dumped to `dist` folder instead (this folder can
      be directly served by nginx in production now)
    - Added Bootstrap to project
    
    Closes #219

--- src-backbone/gulpfile.js
+++ src-backbone/gulpfile.js
@@ -7,26 +7,31 @@
 var gulp = require('gulp');
 var debug = false;
 
-var config = {
-  main: 'app/js/main.coffee',
-  javascripts: [
-    'app/js/**/*.js',
-  ],
-  coffeescripts: [
-    'app/js/**/*.coffee',
-  ],
-  handlebars: [
-    'app/js/**/*.hbs',
-  ],
-  images: [
-    'app/img/**/*'
-  ],
-  stylesheets: [
-    'app/css/**/*.less', // Since LESS is a superset of CSS, the preprocessor shouldn't affect regular CSS files
-    'app/css/**/*.css',
-  ],
-  bower: 'bower_components/**',
-  output: 'app/assets'
+var Config = {
+    main_app: [
+        'app/js/main.coffee',
+    ],
+    javascripts: [
+        'app/js/**/*.js',
+    ],
+    coffeescripts: [
+        'app/js/**/*.coffee',
+    ],
+    handlebars: [
+        'app/js/**/*.hbs',
+    ],
+    stylesheets: [
+        'app/css/**/*.less', // Since LESS is a superset of CSS, the preprocessor shouldn't affect regular CSS files
+        'app/css/**/*.css',
+    ],
+    images: [
+        'app/img/**/*'
+    ],
+    html: [
+        'app/index.html'
+    ],
+    bower_components: 'bower_components/**',
+    output: 'dist'
 };
 
 //------------------------------------------------------------------------------
@@ -35,11 +40,11 @@ var config = {
 
 var connect = require('gulp-connect');
 gulp.task('webserver', function() {
-  connect.server({
-    livereload: true,
-    root: ['app'],
-    fallback: 'app/index.html'
-  });
+    connect.server({
+        livereload: true,
+        root: [ Config.output ],
+        fallback: Config.output + '/index.html'
+    });
 });
 
 //------------------------------------------------------------------------------
@@ -52,29 +57,14 @@ var source_maps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 
 gulp.task('css', function() {
-  gulp.src(config.stylesheets)
-    .pipe(source_maps.init())
-    .pipe(less())
-    .pipe(concat('app.css'))
-    .pipe(minify_css())
-    .pipe(source_maps.write())
-    .pipe(gulp.dest(config.output))
-    .pipe(connect.reload());
-});
-
-//------------------------------------------------------------------------------
-// Bower Tasks
-//------------------------------------------------------------------------------
-
-var main_bower_files = require('main-bower-files');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-
-gulp.task('bower', function() {
-  gulp.src(main_bower_files())
-    .pipe(concat('libs.js'))
-    .pipe(gulpif(!debug, uglify()))
-    .pipe(gulp.dest(config.output));
+    gulp.src(Config.stylesheets)
+        .pipe(source_maps.init())
+        .pipe(less())
+        .pipe(concat('app.css'))
+        .pipe(minify_css())
+        .pipe(gulpif(!debug, source_maps.write()))
+        .pipe(gulp.dest(Config.output + '/css'))
+        .pipe(connect.reload());
 });
 
 //------------------------------------------------------------------------------
@@ -85,33 +75,33 @@ var jshint = require('gulp-jshint'); // Note: we're using JSHint, a better fork 
 var stylish = require('jshint-stylish');
 
 gulp.task('js-lint', function() {
-  gulp.src(config.javascripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    gulp.src(Config.javascripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
 });
 
 var coffeelint = require('gulp-coffeelint');
 
 gulp.task('cs-lint', function() {
-  gulp.src(config.coffeescripts)
-    .pipe(coffeelint())
-    .pipe(coffeelint.reporter());
+    gulp.src(Config.coffeescripts)
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter());
 });
 
 var coffeeify = require('gulp-coffeeify');
 var rename = require("gulp-rename");
 
 gulp.task('js-coffee', function() {
-  gulp.src(config.main)
-    .pipe(coffeeify({
-      options: {
-        debug: true, // Generate source map 
-        paths: [ __dirname + '/app/js' ]
-      }
-    }))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest(config.output))
-    .pipe(connect.reload());
+    gulp.src(Config.main_app)
+        .pipe(coffeeify({
+            options: {
+                debug: true, // Generate source map 
+                paths: [ __dirname + '/app/js' ]
+            }
+        }))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest(Config.output + '/js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('js', ['js-lint', 'cs-lint', 'js-coffee']);
@@ -125,39 +115,117 @@ var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
 
-gulp.task('hbs', function(){
-  gulp.src(config.handlebars)
-    .pipe(handlebars({
-      handlebars: require('handlebars')
-    }))
-    .pipe(wrap('(function() { var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {}; templates[\'<%= getName(file.relative) %>\'] = template(<%= contents %>);})();', {}, {
-      imports: {
-        getName: function(filename) {
-          var path = require('path');
-          return path.basename(filename, path.extname(filename));
-        }
-      }
-    }))
-    .pipe(declare({
-      namespace: 'Sana.templates',
-      noRedeclare: true, // Avoid duplicate declarations 
-    }))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest(config.output))
-    .pipe(connect.reload());
+gulp.task('hbs', function() {
+    gulp.src(Config.handlebars)
+        .pipe(handlebars({
+            handlebars: require('handlebars')
+        }))
+        .pipe(wrap('(function() { var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {}; templates[\'<%= getName(file.relative) %>\'] = template(<%= contents %>);})();', {}, {
+            imports: {
+                getName: function(filename) {
+                    var path = require('path');
+                    return path.basename(filename, path.extname(filename));
+                }
+            }
+        }))
+        .pipe(declare({
+            namespace: 'Sana.templates',
+            noRedeclare: true, // Avoid duplicate declarations 
+        }))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest(Config.output + '/js'))
+        .pipe(connect.reload());
 });
+
+//------------------------------------------------------------------------------
+// Image Tasks
+//------------------------------------------------------------------------------
+
+gulp.task('img', function() {
+    gulp.src(Config.images)
+        .pipe(gulp.dest(Config.output + '/img'))
+        .pipe(connect.reload());
+});
+
+//------------------------------------------------------------------------------
+// HTML Tasks
+//------------------------------------------------------------------------------
+
+gulp.task('html', function() {
+    gulp.src(Config.html)
+        .pipe(gulp.dest(Config.output))
+        .pipe(connect.reload());
+});
+
+//------------------------------------------------------------------------------
+// Bower Tasks
+//------------------------------------------------------------------------------
+
+var main_bower_files = require('main-bower-files');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
+var flatten = require('gulp-flatten');
+
+var bower_files = main_bower_files({
+    overrides: {
+        bootstrap: {
+            main: [
+                './dist/js/bootstrap.js',
+                './dist/css/*.min.*',
+                './dist/fonts/*.*'
+            ]
+        }
+    }
+});
+
+gulp.task('bower-js', function() {
+    var js_filter = filter('**/*.js');
+
+    gulp.src(bower_files, { base: 'bower_components' })
+        .pipe(js_filter)
+        .pipe(concat('libs.js'))
+        .pipe(gulpif(!debug, uglify()))
+        .pipe(gulp.dest(Config.output + '/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('bower-css', function() {
+    var css_filter = filter('**/*.css');
+
+    gulp.src(bower_files, { base: 'bower_components' })
+        .pipe(css_filter)
+        .pipe(concat('libs.css'))
+        .pipe(gulp.dest(Config.output + '/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('bower-fonts', function() {
+    var font_filter = filter('**/dist/fonts/*.*');
+
+    gulp.src(bower_files, { base: 'bower_components' })
+        .pipe(font_filter)
+        .pipe(flatten())
+        .pipe(gulp.dest(Config.output + '/fonts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('bower', ['bower-js', 'bower-css', 'bower-fonts']);
 
 //------------------------------------------------------------------------------
 // Default
 // This executes when you run 'gulp' on the command line
 //------------------------------------------------------------------------------
 
-gulp.task('default', ['css', 'js', 'hbs', 'bower'], function(){
-  gulp.watch(config.stylesheets, ['css']);
-  gulp.watch([config.javascripts, config.coffeescripts], ['js']);
-  gulp.watch(config.handlebars, ['hbs']);
-  gulp.watch(config.bower, ['bower']);
-  gulp.start('webserver');
+gulp.task('default', ['css', 'js', 'hbs', 'bower', 'img', 'html'], function(){
+    gulp.watch(Config.stylesheets, ['css']);
+    gulp.watch([Config.javascripts, Config.coffeescripts], ['js']);
+    gulp.watch(Config.handlebars, ['hbs']);
+    gulp.watch(Config.bower_components, ['bower']);
+    gulp.watch(Config.images, ['img']);
+    gulp.watch(Config.html, ['html']);
+
+    gulp.start('webserver');
 });
 
 //------------------------------------------------------------------------------
@@ -165,9 +233,9 @@ gulp.task('default', ['css', 'js', 'hbs', 'bower'], function(){
 //------------------------------------------------------------------------------
 
 gulp.task('set-debug', function(){
-  debug = true;
+    debug = true;
 })
 
 gulp.task('debug', ['set-debug'], function(){
-  gulp.start('default');
+    gulp.start('default');
 })
