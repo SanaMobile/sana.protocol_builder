b56c5ca12f9806ecedc531e1f00ec1d7f2162b46
Connor Cimowsky <connorcimowsky@me.com>
2016-02-19 21:51:47 -0500

    Add an endpoint for email confirmation

--- src-django/authentication/urls.py
+++ src-django/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from views import login, logout, signup
+from views import login, logout, signup, confirm_email
 
 
 urlpatterns = [
     url(r'^login', login),
     url(r'^logout', logout),
     url(r'^signup', signup),
+    url(r'^confirm_email/(?P<key>\w+)', confirm_email),
 ]
