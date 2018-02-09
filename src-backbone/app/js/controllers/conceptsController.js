6e27a58bff30c6e6f12096253d72159222454e30
George <j449li.github@gmail.com>
2018-02-08 21:52:33 -0500

    setup nav bar button and route for concepts manager

--- src-backbone/app/js/controllers/conceptsController.js
+++ src-backbone/app/js/controllers/conceptsController.js
@@ -17,11 +17,24 @@ module.exports = Marionette.Controller.extend({
             Helpers.navigateToDefaultLoggedIn();
             return;
         }
-        Helpers.arrivedOnView('Concepts');
+        Helpers.arrivedOnView('Concepts Uploader');
 
         let conceptsView = new ConceptsLayout();
         App().RootView.clearNotifications(); // Clear login errors
         App().RootView.switchMainView(conceptsView, 'concepts');
         conceptsView.showChildView('conceptFormArea', new ConceptsUploaderView());
     },
+
+    routeManager: function () {
+        if (!App().session.isValid()) {
+            Helpers.navigateToDefaultLoggedOut();
+            return;
+        }
+
+        if (!App().session.user.isPriveleged()) {
+            Helpers.navigateToDefaultLoggedIn();
+            return;
+        }
+        Helpers.arrivedOnView('Concepts Manager');
+    }
 });
