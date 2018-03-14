970c5e8c7fec0c0de4f98c39c7c0083d30ed5488
GiliamMeeussen <giliam.m27@gmail.com>
2018-03-14 01:23:13 -0400

    Cleaned up versioning code

--- src-backbone/app/js/views/builder/builderSelectVersionView.js
+++ src-backbone/app/js/views/builder/builderSelectVersionView.js
@@ -8,18 +8,6 @@ module.exports = Marionette.ItemView.extend({
     tagName: 'option',
     className: 'procedure',
 
-    // value: model.id,
-
-    ui: {
-        versionSelector: 'select#builder-select-version-view',
-    },
-
-    value: 5,
-
-    events: {
-        'change @ui.versionSelector': '_selectMyVersion',
-    },
-
     initialize: function(options){
         this.selectedVersion = options.selectedVersion;
     },
@@ -31,36 +19,4 @@ module.exports = Marionette.ItemView.extend({
             this.$el.attr("selected", "selected");
         }
     },
-
-    // _onDownloadProcedure: function(event) {
-    //     event.preventDefault();
-    //     this.model.generate();
-    // },
-
-    // _onDeleteProcedure: function(event) {
-    //     event.preventDefault();
-
-    //     // TODO prompt user for confirmation
-
-    //     let self = this;
-    //     let el = this.$el;
-
-    //     el.fadeOut(function() {
-    //         self.model.destroy({
-    //             success: function(model, response, options) {
-    //                 console.info('Deleted Procedure', self.model.get('id'));
-    //             },
-    //             error: function(model, response, options) {
-    //                 console.warn('Failed to delete Procedure', self.model.get('id'), response.responseJSON);
-    //                 App().RootView.showNotification('Failed to delete Procedure!');
-    //                 el.fadeIn();
-    //             },
-    //         });
-    //     });
-    // },
-
-    _selectMyVersion: function(event) {
-        console.log("i cry");
-    },
-
 });
