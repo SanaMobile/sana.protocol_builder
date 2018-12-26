b042925af780626456556f3cf7c5c3ba99ec13fc
Michael Socha <msocha@uwaterloo.ca>
2018-12-26 09:51:14 -1000

    Remove language selector require

--- src-backbone/app/js/behaviors/rightNavbarBehavior.js
+++ src-backbone/app/js/behaviors/rightNavbarBehavior.js
@@ -1,5 +1,4 @@
 const App = require('utils/sanaAppInstance');
-const LanguageSelectorView = require('views/common/languageSelectorView');
 const LoggedInRightNavbarView = require('views/common/loggedInRightNavbarView');
 const LoggedOutRightNavbarView = require('views/common/loggedOutRightNavbarView');
 
@@ -7,8 +6,6 @@ const LoggedOutRightNavbarView = require('views/common/loggedOutRightNavbarView'
 module.exports = Marionette.Behavior.extend({
 
     onBeforeShow: function() {
- //       this.view.showChildView('languageSelector', new LanguageSelectorView());
-
         if (App().session.isValid()) {
             this.view.showChildView('rightNavbar', new LoggedInRightNavbarView());
         } else {
